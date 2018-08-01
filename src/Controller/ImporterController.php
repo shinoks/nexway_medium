855a7aa756afaee4502d42781f508d3044824b60
Shinoks <shinoks@o2.pl>
2018-08-01 13:59:51 +0200

    Start for unit tests and sample files added

--- src/Controller/ImporterController.php
+++ src/Controller/ImporterController.php
@@ -10,13 +10,13 @@ class ImporterController
     public function index()
     {
         $import = new ImportPrices();
-        $fileJson = new File($_SERVER['DOCUMENT_ROOT'].'/../var/data/test.json');
-        $fileJsonZip = new File($_SERVER['DOCUMENT_ROOT'].'/../var/data/testJson.zip');
-        $fileAllZip = new File($_SERVER['DOCUMENT_ROOT'].'/../var/data/testall.zip');
-        $fileAllRar = new File($_SERVER['DOCUMENT_ROOT'].'/../var/data/testrar.rar');
-       // $fileXml = new File($_SERVER['DOCUMENT_ROOT'].'/../var/data/test.xml');
-        //$fileCsv = new File($_SERVER['DOCUMENT_ROOT'].'/../var/data/test.csv');
-        return new Response('<html>'.var_dump($import->import($fileJson)).'<br/>'.var_dump($import->import($fileJsonZip)).'<br/>'.var_dump($import->import($fileAllZip)).'<br/>'.var_dump($import->import($fileAllRar)).'<br/>');
-        //return new Response('<html>'.var_dump($import->import($fileJson)).'<br/>'.var_dump($import->import($fileJsonZip)).'<br/>'.var_dump($import->import($fileXml)).'<br/>'.var_dump($import->import($fileCsv)).'</html>');
+        $fileJson = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/test.json');
+        $fileJsonZip = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/testJson.zip');
+        $fileAllZip = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/testall.zip');
+        $fileAllRar = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/testrar.rar');
+        $fileXml = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/test.xml');
+        $fileCsv = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/test.csv');
+
+        return new Response('<html>'.var_dump($import->import($fileJson)).'<br/>'.var_dump($import->import($fileJsonZip)).'<br/>'.var_dump($import->import($fileAllZip)).'<br/>'.var_dump($import->import($fileAllRar)).'<br/>'.var_dump($import->import($fileXml)).'<br/>'.var_dump($import->import($fileCsv)));
     }
 }
