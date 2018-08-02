40754ad21aec049961fbf36c9a8c017d89f73a75
Shinoks <shinoks@o2.pl>
2018-08-02 14:43:52 +0200

    Import page added

--- src/Controller/ImporterController.php
+++ src/Controller/ImporterController.php
@@ -2,10 +2,14 @@
 namespace App\Controller;
 
 use App\Utils\ImportPrices;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
-class ImporterController
+class ImporterController extends Controller
 {
     public function index()
     {
@@ -18,5 +22,41 @@ class ImporterController
         $fileCsv = new File($_SERVER['DOCUMENT_ROOT'].'/../tests/sample/test.csv');
 
         return new Response('<html>'.var_dump($import->import($fileJson)).'<br/>'.var_dump($import->import($fileJsonZip)).'<br/>'.var_dump($import->import($fileAllZip)).'<br/>'.var_dump($import->import($fileAllRar)).'<br/>'.var_dump($import->import($fileXml)).'<br/>'.var_dump($import->import($fileCsv)));
+    }
+
+    /**
+     * //Page for importing
+     * @param Request $request
+     * @return Response
+     */
+    public function importPage(Request $request)
+    {
+        $mess = ['message' => 'Import File' ];
+        $form = $this->createFormBuilder($mess)
+        ->add('file',FileType::class,['label' => 'Upload file with petrol prices'])
+        ->add('save', SubmitType::class, array('label' => 'Submit file'))
+        ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form['file']->getData();
+            $tempDir = $_SERVER['DOCUMENT_ROOT'].'/../var/data/';
+            $name = md5(rand(1, 99999)).$file->getClientOriginalName();
+            $file->move($tempDir, $name);
+            $f = new File($tempDir.$name);
+            $import = new ImportPrices();
+            $decodedFile = $import->import($f);
+            unlink($_SERVER['DOCUMENT_ROOT'].'/../var/data/'.$name);
+
+            return $this->render('import_page.html.twig', array(
+                'form' => $form->createView(),
+                'decoded_file' => $decodedFile
+            ));
+        }
+
+        return $this->render('import_page.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
